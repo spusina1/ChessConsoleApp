48306826bb99a8d1542e04be8c19e87141d6d96f
spusina1 <spusina1@etf.unsa.ba>
2018-11-10 20:28:09 +0100

    Dodala prvu move metodu i pomocne metode

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -52,5 +52,173 @@ public class Board {
     }
 
 
+    private void provjeriHorizontalno(int i, int j, int novoJ){
+        if(j<novoJ)
+            for(int k=j+1; k<novoJ; k++)
+                if(chessBoard[i][k]!=null) throw izuzetak;
+         else
+             for (int n=novoJ+1; n<j; n++)
+                 if(chessBoard[i][n]!=null) throw izuzetak;
+    }
+
+    private void provjeriVertikalno(int i, int novoI, int j){
+        if(i<novoI){
+            for(int k=i+1; k<novoI; k++)
+                if(chessBoard[k][j]!=null) throw izuzetak;}
+        else {
+            for (int n=novoI+1; n<i; n++)
+                if(chessBoard[n][j] !=null) throw izuzetak;
+        }
+
+    }
+
+    private  void provjeriDijagonalni(int i, int j, int novoI, int novoJ) {
+        int x = 0;
+        int y = 0;
+        if (i < novoI && j < novoJ) {
+             x = i + 1;
+             y = j + 1;
+            while (x != novoI) {
+                if (chessBoard[x][y] != null) throw izuzetak;
+                x++;
+                y++;
+            }
+        } else if (i < novoI && j > novoJ) {
+             x = i + 1;
+             y = j - 1;
+            while (x != novoI) {
+                if (chessBoard[x][y] != null) throw izuzetak;
+                x++;
+                y--;
+            }
+        } else if (i > novoI && j > novoJ) {
+             x = i - 1;
+             y = j - 1;
+            while (x != novoI) {
+                if (chessBoard[x][y] != null) throw izuzetak;
+                x--;
+                y--;
+            }
+        } else if (i > novoI && j < novoJ) {
+             x = i-1;
+             y = j+1;
+            while (x != novoI) {
+                if (chessBoard[x][y] != null) throw izuzetak;
+                x--;
+                y++;
+            }
+        }
+    }
+    private void provjeriVertikalnoZaPijuna(int i, int novoI, int j){
+
+        if(chessBoard[novoI][j]!=null) throw izuzetak;
+        int x=0;
+        int y=0;
+        if(i<novoI){
+            x=i+1;
+            while(x!=novoI){
+                if(chessBoard[x][j]!=null) throw izuzetak;
+                x++;
+            }
+        }
+        else if(i>novoI){
+            x=i-1;
+            while ((x!=novoI)){
+                if(chessBoard[x][j] != null ) throw  izuzetak;
+                x--;
+            }
+        }
+    }
+
+    private void provjeriDijagonalnoZaPijuna(int novoI, int novoJ){
+        if(chessBoard[novoI][novoJ]==null) throw izuzetak;
+
+    }
+
+    public void move(Class type, ChessPiece.Color color, String position){
+
+      int koordinataI=0;
+      int koordinataJ=0;
+        Vanjska2:  for (int i = 0; i < 8; i++) {
+            String str = Integer.toString(i+1);
+            for (int j = 0; j < 8; j++) {
+                String trazenaPozicija = (char) (j + 65) + str;
+                String trazenaPozicija2 = (char) (j + 97) + str;
+                if (trazenaPozicija.equals(position) || trazenaPozicija2.equals(position)) {
+                    if (chessBoard[i][j] != null) {
+                        if (chessBoard[i][j].getColor().equals(color)) {
+                            throw izuzetak;
+                        }
+                    }
+                    koordinataI=i;
+                    koordinataJ=j;
+                    break Vanjska2;
+                }
+            }
+        }
+        int pamtiI=0;
+        int pamtiJ=0;
+      boolean pomjeren=false;
+        Vanjska: for(int i=0; i<8; i++){
+            for(int j=0; j<8; j++) {
+                if(chessBoard[i][j]!=null){
+                if (chessBoard[i][j].getClass() == type && chessBoard[i][j].getColor().equals(color)) {
+                    pamtiI=i;
+                    pamtiJ=j;
+                    try {
+                        if (chessBoard[i][j] instanceof King) {
+                            King klon = (King) chessBoard[i][j].clone();
+                            klon.move(position);
+                            pomjeren = true;
+                            break Vanjska;
+                        } else if (chessBoard[i][j] instanceof Queen) {
+                            Queen klon1 = (Queen) chessBoard[i][j].clone();
+                            klon1.move(position);
+                            if (i == koordinataI) provjeriHorizontalno(i, j, koordinataJ);
+                            else if (j == koordinataJ) provjeriVertikalno(i, koordinataI, j);
+                            else provjeriDijagonalni(i, j, koordinataI, koordinataJ);
+                            pomjeren = true;
+                            break Vanjska;
+                        } else if (chessBoard[i][j] instanceof Knight) {
+                            Knight klon2 = (Knight)chessBoard[i][j].clone();
+                            klon2.move(position);
+                            pomjeren = true;
+                            break Vanjska;
+                        } else if (chessBoard[i][j] instanceof Rook) {
+                            Rook klon3 = (Rook) chessBoard[i][j].clone();
+                            klon3.move(position);
+                            if (i == koordinataI) provjeriHorizontalno(i, j, koordinataJ);
+                            else if (j == koordinataJ) provjeriVertikalno(i, koordinataI, j);
+                            pomjeren = true;
+                            break Vanjska;
+                        } else if (chessBoard[i][j] instanceof Bishop) {
+                            Bishop klon4 = (Bishop) chessBoard[i][j].clone();
+                            klon4.move(position);
+                            provjeriDijagonalni(i, j, koordinataI, koordinataJ);
+                            pomjeren = true;
+                            break Vanjska;
+                        } else if (chessBoard[i][j] instanceof Pawn) {
+                            Pawn klon5 = (Pawn) chessBoard[i][j].clone();
+                            klon5.move(position);
+                            if (j == koordinataJ) provjeriVertikalnoZaPijuna(i, koordinataI, j);
+                            else provjeriDijagonalnoZaPijuna(koordinataI, koordinataJ);
+                            pomjeren = true;
+                            break Vanjska;
+                        }
+
+                    } catch (IllegalArgumentException e) {
+
+                    }
+                }
+
+            }
+            }
+        }
+        if(!pomjeren) throw izuzetak;
+        chessBoard[pamtiI][pamtiJ].move(position);
+        chessBoard[koordinataI][koordinataJ]=chessBoard[pamtiI][pamtiJ];
+        chessBoard[pamtiI][pamtiJ]=null;
+    }
+
 }
 
