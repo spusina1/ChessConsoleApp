3017a7ec108968860bc1c6157c5b20a575fc57fa
spusina1 <spusina1@etf.unsa.ba>
2018-11-10 21:37:46 +0100

    Napisala nove testove

--- test/ba/unsa/etf/rpr/PawnTest.java
+++ test/ba/unsa/etf/rpr/PawnTest.java
@@ -6,9 +6,32 @@ class PawnTest {
 
     @org.junit.jupiter.api.Test
     void move1() {
-        Pawn p = new Pawn("E2", ChessPiece.Color.WHITE);
+        Pawn p = new Pawn("E3", ChessPiece.Color.WHITE);
         assertDoesNotThrow(
                 () -> p.move("E4")
+        );
+    }
+    @org.junit.jupiter.api.Test
+    void move12() {
+        Pawn p = new Pawn("E2", ChessPiece.Color.WHITE);
+        assertThrows( IllegalChessMoveException.class,
+                () -> p.move("E5")
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void move13() {
+        Pawn p = new Pawn("E2", ChessPiece.Color.WHITE);
+        assertThrows( IllegalChessMoveException.class,
+                () -> p.move("C4")
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void move4() {
+        Pawn p = new Pawn("E3", ChessPiece.Color.WHITE);
+        assertDoesNotThrow(
+                () -> p.move("D4")
         );
     }
 
