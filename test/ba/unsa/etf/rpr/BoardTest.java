870ae3d64346731a6f15f05c1100c44ac533afe4
spusina1 <spusina1@etf.unsa.ba>
2018-11-10 21:55:58 +0100

    Novi testovi

--- test/ba/unsa/etf/rpr/BoardTest.java
+++ test/ba/unsa/etf/rpr/BoardTest.java
@@ -85,7 +85,7 @@ class BoardTest {
         try {
             b.move(Pawn.class, ChessPiece.Color.WHITE, "E4");
             b.move(Pawn.class, ChessPiece.Color.WHITE, "E5");
-            b.move(Pawn.class, ChessPiece.Color.WHITE, "E6");
+           b.move(Pawn.class, ChessPiece.Color.WHITE, "E6");
             b.move(Pawn.class, ChessPiece.Color.WHITE, "D7");
             b.move(Pawn.class, ChessPiece.Color.WHITE, "C8");
             b.move(Queen.class, ChessPiece.Color.WHITE, "E2");
