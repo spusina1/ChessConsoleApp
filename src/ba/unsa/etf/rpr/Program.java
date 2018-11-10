013cf943825e4a6dde598218a3feaef673fbae48
spusina1 <spusina1@etf.unsa.ba>
2018-11-10 20:42:00 +0100

    Implementirala klasu program

---
+++ src/ba/unsa/etf/rpr/Program.java
@@ -0,0 +1,144 @@
+package ba.unsa.etf.rpr;
+import java.util.*;
+import ba.unsa.etf.rpr.ChessPiece.Color;
+import javafx.beans.binding.When;
+
+public class Program {
+     public static void main(String[] args) {
+    	Board sahovskaPloca=new Board();
+    	boolean logicka=true;
+    	while(true) {
+
+            while (true) {
+                boolean pomjerenBijeli = false;
+                Scanner ulaz = new Scanner(System.in);
+                System.out.println("Unesite X za predaju!");
+                System.out.println("White move:");
+                String potez = ulaz.next();
+                if(potez.equals("X")){ logicka=false; break;}
+                char karakter;
+                if (potez.length() == 3) {
+                    karakter = potez.charAt(0);
+                    String pozicija = "";
+                    pozicija += potez.charAt(1);
+                    pozicija += potez.charAt(2);
+                    try {
+                        switch (karakter) {
+                            case 'K':
+                                sahovskaPloca.move(King.class, Color.WHITE, pozicija);
+                                pomjerenBijeli = true;
+                                break;
+                            case 'Q':
+                                sahovskaPloca.move(Queen.class, Color.WHITE, pozicija);
+                                pomjerenBijeli = true;
+                                break;
+
+                            case 'R':
+                                sahovskaPloca.move(Rook.class, Color.WHITE, pozicija);
+                                pomjerenBijeli = true;
+                                break;
+                            case 'N':
+                                sahovskaPloca.move(Knight.class, Color.WHITE, pozicija);
+                                pomjerenBijeli = true;
+                                break;
+                            case 'B':
+                                sahovskaPloca.move(Bishop.class, Color.WHITE, pozicija);
+                                pomjerenBijeli = true;
+                                break;
+                            default:
+                                System.out.println("Illegal move!");
+                        }
+                    } catch (IllegalArgumentException i) {
+
+                    }
+                    if (!pomjerenBijeli) System.out.println("Illegal move!");
+                    else break;
+                } else {
+                    Petlja:
+                    for (int i = 0; i < 8; i++) {
+                        for (int j = 0; j < 8; j++) {
+                            if (sahovskaPloca.chessBoard[i][j] != null)
+                                if (sahovskaPloca.chessBoard[i][j].getColor().equals(Color.WHITE)) {
+                                    try {
+                                        sahovskaPloca.move(sahovskaPloca.chessBoard[i][j].getClass(), Color.WHITE, potez);
+                                        pomjerenBijeli = true;
+                                        break Petlja;
+                                    } catch (IllegalArgumentException e) {
+
+                                    }
+                                }
+                        }
+                    }
+                    if (!pomjerenBijeli) System.out.println("Illegal move!");
+                    else break;
+                }
+            }
+            if(!logicka) {System.out.println("Bijeli igrac se predao"); break;}
+            while (true) {
+                boolean pomjerenCrni = false;
+                Scanner ulaz = new Scanner(System.in);
+                System.out.println("Unesite X za predaju!");
+                System.out.println("Black move:");
+                String potez = ulaz.next();
+                if(potez.equals("X")){ logicka=false; break;}
+                char karakter;
+                if (potez.length() == 3) {
+                    karakter = potez.charAt(0);
+                    String pozicija = "";
+                    pozicija += potez.charAt(1);
+                    pozicija += potez.charAt(2);
+                    try {
+                        switch (karakter) {
+                            case 'K':
+                                sahovskaPloca.move(King.class, Color.BLACK, pozicija);
+                                pomjerenCrni = true;
+                                break;
+                            case 'Q':
+                                sahovskaPloca.move(Queen.class, Color.BLACK, pozicija);
+                                pomjerenCrni = true;
+                                break;
+
+                            case 'R':
+                                sahovskaPloca.move(Rook.class, Color.BLACK, pozicija);
+                                pomjerenCrni = true;
+                                break;
+                            case 'N':
+                                sahovskaPloca.move(Knight.class, Color.BLACK, pozicija);
+                                pomjerenCrni = true;
+                                break;
+                            case 'B':
+                                sahovskaPloca.move(Bishop.class, Color.BLACK, pozicija);
+                                pomjerenCrni = true;
+                                break;
+                            default:
+                                System.out.println("Illegal move!");
+                        }
+                    } catch (IllegalArgumentException i) {
+
+                    }
+                    if (!pomjerenCrni) System.out.println("Illegal move!");
+                    else break;
+                } else {
+                    Petlja:
+                    for (int i = 0; i < 8; i++) {
+                        for (int j = 0; j < 8; j++) {
+                            if (sahovskaPloca.chessBoard[i][j] != null)
+                                if (sahovskaPloca.chessBoard[i][j].getColor().equals(Color.BLACK)) {
+                                    try {
+                                        sahovskaPloca.move(sahovskaPloca.chessBoard[i][j].getClass(), Color.BLACK, potez);
+                                        pomjerenCrni= true;
+                                        break Petlja;
+                                    } catch (IllegalArgumentException e) {
+
+                                    }
+                                }
+                        }
+                    }
+                    if (!pomjerenCrni) System.out.println("Illegal move!");
+                    else break;
+                }
+            }
+            if(!logicka) {System.out.println("Crni igrac se predao"); break;}
+        }
+    }
+}
